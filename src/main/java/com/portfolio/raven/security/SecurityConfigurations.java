ff2cd95c75dd0ac3877f5adab0607b8431aaf433
Edurb23 <eddurb64@gmail.com>
2026-07-02 01:49:55 -0300

    Testes Unitario e Role guards

--- src/main/java/com/portfolio/raven/security/SecurityConfigurations.java
+++ src/main/java/com/portfolio/raven/security/SecurityConfigurations.java
@@ -3,7 +3,6 @@ package com.portfolio.raven.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,21 +33,13 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/user/{id}").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/artist/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/user/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/user/listAll").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/user/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/artist/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/artist/upload/imagem").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/artists/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/artist/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/genre/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/genre/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/genre/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/genre/**").hasRole("ADMIN")
+                        .requestMatchers(
+                                "/login/**",
+                                "/user/register",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
